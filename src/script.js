af47e0cf49e795028adcb37e92643134595595b8
Jonas Deprund <jonasdeprund@gmail.com>
2021-09-29 18:27:44 +0200

    raycaster wip

--- src/script.js
+++ src/script.js
@@ -43,14 +43,6 @@ scene.add(object1, object2, object3);
 
 const raycaster = new THREE.Raycaster();
 
-const rayOrigin = new THREE.Vector3(-3, 0, 0);
-const rayDirection = new THREE.Vector3(10, 0, 0);
-rayDirection.normalize();
-
-raycaster.set(rayOrigin, rayDirection);
-
-const intersect = raycaster.intersectObject(object2);
-
 /**
  * Sizes
  */
@@ -106,6 +98,27 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  // Animate object
+  object1.position.y = Math.sin(elapsedTime * 0.5) * 1.5;
+  object2.position.y = Math.sin(elapsedTime * 0.8) * 1.5;
+  object3.position.y = Math.sin(elapsedTime * 1.2) * 1.5;
+
+  // Cast a ray
+  const rayOrigin = new THREE.Vector3(-3, 0, 0);
+  const rayDirection = new THREE.Vector3(1, 0, 0);
+  rayDirection.normalize();
+
+  raycaster.set(rayOrigin, rayDirection);
+
+  const objectsToTest = [object1, object2, object3];
+  const intersects = raycaster.intersectObjects(objectsToTest);
+
+  for (const intersect of intersects) {
+    intersect.object.material.color.set('#0000ff');
+  }
+
+  // console.log(intersects.length);
 
   // Update controls
   controls.update();
